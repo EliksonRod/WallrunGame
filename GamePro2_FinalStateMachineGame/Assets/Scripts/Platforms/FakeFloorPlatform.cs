f8ed52d098f250cc11321b1df924128c88604f62
WittlyEli <162608185+EliRod61@users.noreply.github.com>
2025-08-28 04:18:22 -0400

    Split platforms updated player detection

--- GamePro2_FinalStateMachineGame/Assets/Scripts/Platforms/FakeFloorPlatform.cs
+++ GamePro2_FinalStateMachineGame/Assets/Scripts/Platforms/FakeFloorPlatform.cs
@@ -19,8 +19,10 @@ public class FakeFloorPlatform : MonoBehaviour
 
     [SerializeField] Transform Player_Transform;
     [SerializeField]Vector3 Original_Position;
-    [SerializeField] Vector3 Target_Position;
+    [SerializeField]Vector3 Target_Position;
     float distanceFromPlayer;
+    float distanceFromOriginalLocation;
+    bool player_In_Range = false;
 
     void Start()
     {
@@ -31,19 +33,28 @@ public class FakeFloorPlatform : MonoBehaviour
         KeepPlayerOnPlaform();
 
         distanceFromPlayer = Vector3.Distance(Player_Transform.position, gameObject.transform.position);
+        distanceFromOriginalLocation = Vector3.Distance(Player_Transform.position, Original_Position);
 
-        //If player is further than 10 units away, go to original position, else go to target position
-        if (distanceFromPlayer >= detectionRange)
+        //If player is far enough from platform or platform's original location, platform returns to original location
+        if (distanceFromPlayer >= detectionRange && distanceFromOriginalLocation >= detectionRange)
         {
-            transform.position = Vector3.MoveTowards(transform.position, Original_Position, Speed);
-            //transform.Translate(Original_Position * Time.deltaTime);
+            player_In_Range = false;
+        }
+        else if (distanceFromPlayer <= detectionRange || distanceFromOriginalLocation <= detectionRange)
+        {
+            player_In_Range = true;
+        }
+
+
+        if (player_In_Range == true)
+        {
+            Debug.Log("Player in range");
+            transform.position = Vector3.MoveTowards(transform.position, Target_Position, Speed);
         }
         else
         {
-            //transform.Translate(Target_Position * Time.deltaTime);
-            transform.position = Vector3.MoveTowards(transform.position, Target_Position, Speed);
+            transform.position = Vector3.MoveTowards(transform.position, Original_Position, Speed);
         }
-
     }
     void OnCollisionEnter(Collision other)
     {
