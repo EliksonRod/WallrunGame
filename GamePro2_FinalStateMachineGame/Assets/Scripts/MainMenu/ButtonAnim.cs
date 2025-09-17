d3a75205268fc89aac891b00e5ba4801f8a52d20
WittlyEli <162608185+EliRod61@users.noreply.github.com>
2025-09-17 02:11:20 -0400

    Renamed some files, new main menu and animations

--- GamePro2_FinalStateMachineGame/Assets/Scripts/MainMenu/ButtonAnim.cs
+++ GamePro2_FinalStateMachineGame/Assets/Scripts/MainMenu/ButtonAnim.cs
@@ -10,22 +10,35 @@ public class ButtonAnim : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     public Animator animWhenHoverOver;
 
     bool mouseIsOverUI = false;
+    bool animPlayed = false;
+
+    void Start()
+    {
+        animWhenHoverOver.enabled = false;
+    }
     private void FixedUpdate()
     {
         if (mouseIsOverUI == false)
         {
-            animWhenHoverOver.enabled = false;
+            //animWhenHoverOver.enabled = false;
         }
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
-        mouseIsOverUI = true;
         animWhenHoverOver.enabled = true;
-        animWhenHoverOver.Play("HoverOverGrowShrink");
+        mouseIsOverUI = true;
+
+        if (animPlayed != true)
+        {
+            animWhenHoverOver.enabled = true;
+            animWhenHoverOver.Play("HoverOverGrowShrink");
+            animPlayed = true;
+        }   
     }
     public void OnPointerExit(PointerEventData eventData)
     {
         mouseIsOverUI = false;
+        animPlayed = false;
         animWhenHoverOver.Play("ReturnToNormalSize");   
     }
     public void returnSize()
